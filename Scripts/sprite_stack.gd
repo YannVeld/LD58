f4768fd06a7ba9e59ab6bd6b39fd0ba429c1a460
Yann in 't Veld <yannintveld@gmail.com>
2025-10-04 15:07:59 +0200

    New sprite stacked car

--- Scripts/sprite_stack.gd
+++ Scripts/sprite_stack.gd
@@ -3,19 +3,25 @@ extends Node2D
 @export var sprites: Array[Texture2D]
 @export var offset: float = 1.0
 @export var myScale: Vector2 = Vector2(1,1)
+@export var reverseOrder: bool = false
+@export var layerCopies: int = 1
 
 var myRenderers: Array[Sprite2D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	myRenderers.resize((sprites.size()))
-	for _n in range(sprites.size()):
+	if reverseOrder: sprites.reverse()
+	
+	myRenderers.resize((sprites.size() * layerCopies))
+	for _n in range(sprites.size() * layerCopies):
 		var sprite2d = Sprite2D.new()
 		add_child(sprite2d)
 		
 		sprite2d.global_position = global_position
-		sprite2d.texture = sprites[_n]
+		
+		var spriteInd: int = floor( _n / layerCopies )
+		sprite2d.texture = sprites[spriteInd]
 		sprite2d.position.y -= offset * _n
 		sprite2d.scale = myScale
 	
