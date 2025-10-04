6b5b0d9e1d789edbcda87d97bfd3c9e0a575b59f
Gautam Rai <69165693+gautamra@users.noreply.github.com>
2025-10-04 17:56:17 +0200

    Add partial fix to drifting issue

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
-@export var wheel_base = 40
-@export var steering_angle = 15
-@export var engine_power = 9000
+@export var wheel_base = 15
+@export var steering_angle = 10
+@export var engine_power = 500
 @export var braking = -450
 @export var max_speed_reverse = 250
 @export var min_speed = 25
-@export var drift_threshold_speed = 400
-@export var traction_fast = 0.1
-@export var traction_slow = 0.9
+@export var drift_threshold_speed = 100
+@export var traction_fast = 0.0
+@export var traction_slow = 0.7
 
-var friction = -55*0.02
+var friction = -55/110.0
 var drag = -0.06
 
 var acceleration = Vector2.ZERO
@@ -19,6 +19,7 @@ var steer_direction
 func get_input():
 	var turn = Input.get_axis("steer_left", "steer_right")
 	steer_direction = turn * deg_to_rad(steering_angle)
+	print(steer_direction)
 	if Input.is_action_pressed("accelerate"):
 		acceleration = transform.x * engine_power
 	if Input.is_action_pressed("brake"):
@@ -37,21 +38,27 @@ func calculate_steering(delta):
 		
 	rear_wheel += velocity*delta
 	front_wheel += velocity.rotated(steer_direction)*delta
+	var new_heading = rear_wheel.direction_to(front_wheel)
 	
 	var traction = traction_slow
 	if velocity.length() > drift_threshold_speed:
 		traction = traction_fast
 		print("drifting")
-	velocity += (velocity + velocity.lerp(velocity.rotated(steer_direction)*delta, traction))*delta/2
-	
-	var new_heading = rear_wheel.direction_to(front_wheel)
+
+	velocity = velocity.lerp(new_heading*velocity.length(), traction)
 	rotation = new_heading.angle()
+	
+	var d = new_heading.dot(velocity.normalized())
+	if d < 0:
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
 
 func _physics_process(delta: float) -> void:
 	acceleration = Vector2.ZERO
 	get_input()
 	apply_friction(delta)
+	print(acceleration)
 	velocity += acceleration * delta
 	if velocity.length()>0:
 		calculate_steering(delta)
 	move_and_slide()
+	print("velocity = ", velocity.length() )
