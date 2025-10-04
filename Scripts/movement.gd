6f7aa1e4e4dc8c1d0d491fd660ec5976d3f95c49
Gautam Rai <69165693+gautamra@users.noreply.github.com>
2025-10-04 16:12:12 +0200

    Add drifting functionality

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
 @export var wheel_base = 40
-@export var steering_angle = 30
-@export var engine_power = 9000*1.5
+@export var steering_angle = 15
+@export var engine_power = 9000
 @export var braking = -450
 @export var max_speed_reverse = 250
 @export var min_speed = 25
+@export var drift_threshold_speed = 400
+@export var traction_fast = 0.1
+@export var traction_slow = 0.9
 
-var friction = -55*0.1
+var friction = -55*0.02
 var drag = -0.06
 
 var acceleration = Vector2.ZERO
@@ -21,30 +24,28 @@ func get_input():
 	if Input.is_action_pressed("brake"):
 		acceleration = transform.x * braking
 		
-func calculate_steering(delta):
-	var rear_wheel = position - transform.x * wheel_base/2.0
-	var front_wheel = position + transform.x * wheel_base/2.0
-	
-	rear_wheel += velocity * delta
-	front_wheel += velocity.rotated(steer_direction) * delta
-	print("wheel_base = ", (front_wheel - rear_wheel).length())
-	print("velocity = ", (velocity).length())
-
-	
-	var new_heading = rear_wheel.direction_to(front_wheel)
-	var d = new_heading.dot(velocity.normalized())
-	if d>0:
-		velocity = new_heading * velocity.length()
-	elif d<0:
-		velocity = -new_heading * min(velocity.length(), max_speed_reverse)	
-	rotation = new_heading.angle()
-	
 func apply_friction(delta):
 	if acceleration == Vector2.ZERO and velocity.length() < min_speed:
 		velocity = Vector2.ZERO
 	var friction_force = velocity * friction * delta
 	var drag_force = velocity * velocity.length() * friction * delta
 	acceleration += drag_force + friction_force
+		
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base/2.0
+	var front_wheel = position + transform.x * wheel_base/2.0
+		
+	rear_wheel += velocity*delta
+	front_wheel += velocity.rotated(steer_direction)*delta
+	
+	var traction = traction_slow
+	if velocity.length() > drift_threshold_speed:
+		traction = traction_fast
+		print("drifting")
+	velocity += (velocity + velocity.lerp(velocity.rotated(steer_direction)*delta, traction))*delta/2
+	
+	var new_heading = rear_wheel.direction_to(front_wheel)
+	rotation = new_heading.angle()
 
 func _physics_process(delta: float) -> void:
 	acceleration = Vector2.ZERO
@@ -54,17 +55,3 @@ func _physics_process(delta: float) -> void:
 	if velocity.length()>0:
 		calculate_steering(delta)
 	move_and_slide()
-
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
