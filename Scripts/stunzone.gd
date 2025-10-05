eabb7dfd19408cc18f67d7827a9e04ce7393a6c8
Gautam Rai <69165693+gautamra@users.noreply.github.com>
2025-10-05 15:56:26 +0200

    set world boundaries

--- Scripts/stunzone.gd
+++ Scripts/stunzone.gd
@@ -1,12 +1,9 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("building ready")
-	pass # Replace with function body.
+# Called when the node enters the scene tree for the first time
 
 func _on_body_entered(body: Node2D) -> void:
-	print("A building has just been collided with" )
+	print("Something has just been collided with" )
 	body.handle_collision()
 	pass # Replace with function body.
