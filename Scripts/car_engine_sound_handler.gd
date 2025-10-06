b0ee7b32c61f241e97d1998bd2995fb2b6494398
Yann in 't Veld <yannintveld@gmail.com>
2025-10-06 12:51:58 +0200

    Added engine sound

---
+++ Scripts/car_engine_sound_handler.gd
@@ -0,0 +1,33 @@
+extends AudioStreamPlayer
+
+@onready var body: CharacterBody2D = $"../CharacterBody2D"
+@onready var stopSoundPlayer: AudioStreamPlayer = $"./EngineStopSoundPlayer"
+@onready var startSoundPlayer: AudioStreamPlayer = $"./EngineStartSoundPlayer"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _start_playing() -> void:
+	if (not is_playing()) and (not startSoundPlayer.is_playing()):
+		startSoundPlayer.play(0)
+
+func _stop_playing() -> void:
+	if is_playing():
+		stopSoundPlayer.play()
+	
+	set_playing(false)
+	startSoundPlayer.set_playing(false)
+
+func _process(delta: float) -> void:
+	if (Input.is_action_just_pressed("accelerate") or Input.is_action_just_pressed("brake")):
+		_start_playing()
+	if (not Input.is_action_pressed("accelerate")) and (not Input.is_action_pressed("brake")):
+		_stop_playing() 
+
+func _on_engine_start_sound_player_finished() -> void:
+	set_playing(true)
+
+
+func _on_finished() -> void:
+	set_playing(true)
