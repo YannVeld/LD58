e3f1a78ef0060cf8f6215f0f45ae0390d906e3ed
Yann in 't Veld <yannintveld@gmail.com>
2025-10-06 13:54:28 +0200

    Increase engine sound pitch when speed boosted

--- Scripts/car_engine_sound_handler.gd
+++ Scripts/car_engine_sound_handler.gd
@@ -4,6 +4,9 @@ extends AudioStreamPlayer
 @onready var stopSoundPlayer: AudioStreamPlayer = $"./EngineStopSoundPlayer"
 @onready var startSoundPlayer: AudioStreamPlayer = $"./EngineStartSoundPlayer"
 
+@export var pitchScaleWithBoost: float = 0.8
+@onready var normalPitch = get_pitch_scale()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -19,7 +22,22 @@ func _stop_playing() -> void:
 	set_playing(false)
 	startSoundPlayer.set_playing(false)
 
+func _set_pitch() -> void:
+	set_pitch_scale(normalPitch)
+	stopSoundPlayer.set_pitch_scale(normalPitch)
+	startSoundPlayer.set_pitch_scale(normalPitch)
+	
+	if not carBody.speedupTimer:
+		return
+	if carBody.speedupTimer.time_left > 0:
+		set_pitch_scale(pitchScaleWithBoost)
+		stopSoundPlayer.set_pitch_scale(pitchScaleWithBoost)
+		startSoundPlayer.set_pitch_scale(pitchScaleWithBoost)
+	
+
 func _process(delta: float) -> void:
+	_set_pitch()
+	
 	if (not Input.is_action_pressed("accelerate")) and (not Input.is_action_pressed("brake")):
 		_stop_playing() 
 		return
