f7219a047587f0012a417fa707f672054af0b0bd
Yann in 't Veld <yannintveld@gmail.com>
2025-10-06 20:37:35 +0200

    Updated gameover screen

--- Scripts/game_over_screen.gd
+++ Scripts/game_over_screen.gd
@@ -1,9 +1,16 @@
 extends Control
 
-@onready var score_label: Label = $ScoreLabel
+@onready var score_label: Label = $"./TextBackground/ScoreLabel"
 
 func _ready():
-	score_label.text += str(Global.score)
+	var _txt = str(Global.score) + " pieces of mail collected"
+	score_label.text = _txt
+	
+	#score_label.text += str(Global.score)
 
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://StartScreen.tscn")
+
+
+func _on_restart_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMap.tscn")
